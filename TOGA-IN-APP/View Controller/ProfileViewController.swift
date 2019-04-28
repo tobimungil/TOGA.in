ef9798fdc2dfa20da77f893e8da75a51cf5b3ae6
PramahadiTamaPutra <pramahaditamaputra@outlook.com>
2019-04-28 12:32:13 +0700

    Tambah Card Slider di Profile

---
+++ TOGA-IN-APP/View Controller/ProfileViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ProfileViewController.swift
+//  TOGA-IN-APP
+//
+//  Created by Pramahadi Tama Putra on 28/04/19.
+//  Copyright © 2019 Pramahadi Tama Putra. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProfileCollectionViewCell", for: indexPath) as! ProfieCollectionViewCell
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
